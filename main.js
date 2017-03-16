26e436952c4b95a320ae1a62c256dbb384c4a328
goldspinky <goldspinky@users.noreply.github.com>
2017-03-16 20:55:17 +0000

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function CocoaBeanClick(number){
 var CocoaPods = 0;
 
 function buyCocoaPods(){
-    var CocoaPodCost = Math.floor(10 * Math.pow(1.1,CocoaPod));    	 //works out the cost of this cocoa pod
-    if(CocoaBeans >= CocoaPodCost){                                  	 //checks that the player can afford the cocoa pod
+    var CocoaPodCost = Math.floor(10 * Math.pow(1.1,CocoaPod));    	//works out the cost of this cocoa pod
+    if(CocoaBeans >= CocoaPodCost){                                  	//checks that the player can afford the cocoa pod
         CocoaPod = CocoaPod + 1;                                   	//increases number of cocoa pod
     	CocoaBeans = CocoaBeans - CocoaPodCost;                        	//removes the cocoa beans spent
         document.getElementById('CocoaPod').innerHTML = CocoaPod;  	//updates the number of cocoa pods for the user
@@ -21,6 +21,26 @@ function buyCocoaPods(){
 
 window.setInterval(function(){
 	
-	CocoaBeansClick(cursors);
+	CocoaBeansClick(CocoaPod);
 	
 }, 1000);
+
+var CocoaTree = 0;
+
+function buyCocoaTree(){
+    var CocoaTreeCost = Math.floor(1000 * Math.pow(1.1,CocoaTree));    	//works out the cost of this cocoa tree
+    if(CocoaBeans >= CocoaTreeCost){                                  	//checks that the player can afford the cocoa tree
+        CocoaTree = CocoaTree + 1;                                   	//increases number of cocoa trees
+    	CocoaBeans = CocoaBeans - CocoaTreeCost;                        //removes the cocoa beans spent
+        document.getElementById('CocoaTree').innerHTML = CocoaTree;  	//updates the number of cocoa trees for the user
+        document.getElementById('CocoaPod').innerHTML = CocoaPod;  	//updates the number of cocoa pods for the user
+    };
+    var nextCost = Math.floor(1000 * Math.pow(1.1,CocoaTree));       	//works out the cost of the next cocoa tree
+    document.getElementById('CocoaTreeCost').innerHTML = nextCost; 	//updates the cocoa tree cost for the user
+};
+
+window.setInterval(function(){
+	
+	CocoaPodClick(CocoaTree);
+	
+}, 100000);

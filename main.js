7ee43789b8cbc910042aa520f530c75f7ee5d546
goldspinky <goldspinky@users.noreply.github.com>
2017-03-16 21:41:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -22,20 +22,20 @@ function buycocoapod(){
 };
 
 
-function buycocoatree(){
-    var cocoatreecost = Math.floor(1000 * Math.pow(1.1,cocoatree));    	//works out the cost of this cocoa tree
-    if(cocoabeans >= cocoatreecost){                                  	//checks that the player can afford the cocoa tree
-        cocoatree = cocoatree + 1;                                   	//increases number of cocoa trees
-    	cocoabeans = cocoabeans - cocoatreecost;                        //removes the cocoa beans spent
-        document.getElementById('cocoatree').innerHTML = cocoatree;  	//updates the number of cocoa trees for the user
-        document.getElementById('cocoapod').innerHTML = cocoapod;  	//updates the number of cocoa pods for the user
-    };
-    var nextCost = Math.floor(1000 * Math.pow(1.1,cocoatree));       	//works out the cost of the next cocoa tree
-    document.getElementById('cocoatreecost').innerHTML = nextCost; 	//updates the cocoa tree cost for the user
-};
+//function buycocoatree(){
+//    var cocoatreecost = Math.floor(1000 * Math.pow(1.1,cocoatree));    	//works out the cost of this cocoa tree
+//    if(cocoabeans >= cocoatreecost){                                  	//checks that the player can afford the cocoa tree
+//        cocoatree = cocoatree + 1;                                   	//increases number of cocoa trees
+//    	cocoabeans = cocoabeans - cocoatreecost;                        //removes the cocoa beans spent
+//        document.getElementById('cocoatree').innerHTML = cocoatree;  	//updates the number of cocoa trees for the user
+//        document.getElementById('cocoapod').innerHTML = cocoapod;  	//updates the number of cocoa pods for the user
+//    };
+//    var nextCost = Math.floor(1000 * Math.pow(1.1,cocoatree));       	//works out the cost of the next cocoa tree
+//    document.getElementById('cocoatreecost').innerHTML = nextCost; 	//updates the cocoa tree cost for the user
+//};
 
 window.setInterval(function(){
 	
 	cocoabeanclick(cocoapod);
-	
+//	buycocoapod(cocoatree)	
 }, 1000);
